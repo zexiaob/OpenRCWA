9fcfe718d37abe9602cbe02469da714bd6af6cd1
zexiaob <156300395+zexiaob@users.noreply.github.com>
2025-09-11 12:55:10 +1000

    Adjust wavelength range and solver harmonics for improved simulation performance
    - Changed wavelength range from 900-950 nm to 175-250 nm for finer resolution.
    - Increased the number of points in the wavelength array from 100 to 1000.
    - Reduced the number of harmonics in the solver from (11, 11) to (3, 3) to speed up computations.

--- bic_hbn_metasurface_sim.py
+++ bic_hbn_metasurface_sim.py
@@ -43,7 +43,7 @@ stack = Stack(
 )
 
 
-wavelengths = np.linspace(nm(900), nm(950), 100)  # um, 用较少点加快测试
+wavelengths = np.linspace(nm(175), nm(250), 1000)  # nm, 用较少点加快测试
 TTot_list = []
 RTot_list = []
 
@@ -54,7 +54,7 @@ src = Source(
     pTEM=[0, 1],
 )
 hBN_tensor.source = src
-solver = Solver(layer_stack=stack, source=src, n_harmonics=(11, 11))
+solver = Solver(layer_stack=stack, source=src, n_harmonics=(3, 3))
 
 for wl in wavelengths:
     src.wavelength = wl
